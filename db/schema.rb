bb35000ff7857d3fa02aa999569b06b49d6ab6ae
Mitzi17 <m_estrada17@live.com>
2020-11-11 23:10:48 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_075713) do
+ActiveRecord::Schema.define(version: 2020_11_12_065917) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -24,14 +24,31 @@ ActiveRecord::Schema.define(version: 2020_11_11_075713) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_reviews_on_event_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "reviews", "events"
+  add_foreign_key "reviews", "users"
 end
