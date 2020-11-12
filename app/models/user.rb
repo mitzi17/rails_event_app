75d6d01d5049122e9fdcb1a177e392b8b526ba69
Mitzi17 <m_estrada17@live.com>
2020-11-12 00:12:09 -0800

    add associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+
+  has_many :events
+	has_many :reviews
+	has_many :reviewed_events, through: :reviews, source: :event
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
 end
