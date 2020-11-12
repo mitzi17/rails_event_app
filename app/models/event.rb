83c3fce0fed5beb3d28101ea89bdfd34cb59752a
Mitzi17 <m_estrada17@live.com>
2020-11-12 00:11:37 -0800

    add associations and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  
+  has_many :reviews
+	belongs_to :host, class_name: "User"
+	has_many :reviewers, through: :reviews, source: :user
+
+  validates :name, :location, :category, :price, :location, presence: true
 end
