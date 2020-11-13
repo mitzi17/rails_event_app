e0972e7fab0d4a598fd1a685a6f7021f0b5faa64
Mitzi17 <m_estrada17@live.com>
2020-11-13 14:24:34 -0800

    specify user_id as foreign key in belongs_to association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   
   has_many :reviews
-	belongs_to :host, class_name: "User"
+	belongs_to :host, :class_name => 'User', :foreign_key => 'user_id'
 	has_many :reviewers, through: :reviews, source: :user
 
-  validates :name, :location, :category, :price, :location, presence: true
+  validates :name, :location, :category, :price, :date, presence: true
 end
