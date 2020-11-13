a0a49ee42a3f963d8fa47fa78fc3b2eccd451867
Mitzi17 <m_estrada17@live.com>
2020-11-12 23:51:43 -0800

    add all seven actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,34 @@
 class EventsController < ApplicationController
     
+    before_action :authenticate_user!
+    before_action :set_event, only: [:show, :edit, :update, :destroy]
+
     def index
+        @events = current_user.events
+    end
+
+    def show
+        
     end
     
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_event
+        @event = current_user.events.find(params[:id])
+    end
 end
