264fe13b979995f37e6cd7e20dfb24cb58f562a1
Mitzi17 <m_estrada17@live.com>
2020-11-15 14:12:54 -0800

    define scope method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,12 @@
 class Review < ApplicationRecord
+  
   belongs_to :user
   belongs_to :event
 
   validates :title, :content, presence: true
+
+  def self.most_recent
+    order(updated_at: :desc)
+  end
+
 end
