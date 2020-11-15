ebc75176d65d0bccd01327c31098f65103c17800
Mitzi17 <m_estrada17@live.com>
2020-11-15 14:12:27 -0800

    define scope methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,21 @@ class Event < ApplicationRecord
 	has_many :reviewers, through: :reviews, source: :user
 
   validates :name, :location, :category, :price, :date, presence: true
+
+  def self.upcoming
+    where("date > ?", Time.now)
+  end
+
+  def self.past_event
+    where("date < ?", Time.now)
+  end
+
+  def self.order_by_highest_price
+    order(price: :desc)
+  end
+
+  def self.order_by_lowest_price
+    order(price: :asc)
+  end
+
 end
