d1573bfa5183e1d2c6728023dd6fd6612e1f64c2
Mitzi17 <m_estrada17@live.com>
2020-11-16 00:20:53 -0800

    add rescue for RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    
     def not_found
-        flash[:error] = "Not found"
+        flash[:error] = "Record not found"
         redirect_to root_path
     end
 
