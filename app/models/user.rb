ab72659f664286fb974ca6c947e5116ac094b214
Mitzi17 <m_estrada17@live.com>
2020-11-15 17:45:53 -0800

    add omniauth methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   validates :username, presence: true, uniqueness: true
+
+  def self.from_google(uid:, email:, full_name:, avatar_url:)
+    if user = User.find_by(email: email)
+      user.update(uid: uid, full_name: full_name, avatar_url: avatar_url) unless user.uid.present?
+      user
+    else
+      User.create(
+        email: email,
+        uid: uid,
+        full_name: full_name,
+        avatar_url: avatar_url,
+        password: SecureRandom.hex
+      )
+    end
+  end
+  
 end
